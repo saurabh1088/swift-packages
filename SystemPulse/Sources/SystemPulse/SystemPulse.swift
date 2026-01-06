61809796bf3540c510f17ea6cda98fd856beb7f2
Saurabh Verma <saurabh1088@gmail.com>
2026-01-06 11:45:37 +0530

    Added new swift package to kick off system pulse monitor.

---
+++ SystemPulse/Sources/SystemPulse/SystemPulse.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import ArgumentParser
+import Foundation
+
+@main
+struct SystemPulse {
+    static func main() {
+        print("Hello, world!")
+    }
+}
